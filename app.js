16cbeb24e91b04ed86256eda7a971996b874791e
hoodiebee <uhitmeudie@gmail.com>
2022-06-25 12:24:46 -0700

    app.js

--- app.js
+++ app.js
@@ -1,59 +1,52 @@
-const Rock = "rock";
-const Paper = "paper";
-const Scissors = "scissors";
+let playerScore = 0;
+let computerScore = 0;
 
-function userChoice() {
-  const userChoice = prompt("Do you choose Rock, Paper, or Scissors?").toLowerCase();
-  return userChoice
+function computerPlay() {
+    let allChoices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = allChoices[Math.floor(Math.random() * allChoices.length)];
+    return randomChoice
 }
 
-function compChoice() {
-  let compChoice = Math.random();
-  if (compChoice < 0.34) {
-    compChoice = "rock";
-  } else if (compChoice <= 0.67) {
-    compChoice = "paper";
-  } else {
-    compChoice = "scissors";
-  }
-  return compChoice
+
+function game() {
+    while (playerScore < 5 && computerScore < 5) {
+        const userChoice = prompt("Would you like to choose Rock, Paper or Scissors?")
+        const compChoice = computerPlay();
+        alert(playRound(userChoice, compChoice));
+    }
 }
+
 
 function playRound(userChoice, compChoice) {
-  //Rock Choice
-  if (userChoice == "rock" && compChoice == "scissors") {
-    return "You Win! rock beats scissors!";
-  } else if (userChoice == "rock" && compChoice == "paper") {
-    return "Loser! paper Beats rock";
-  } else if (userChoice == "rock" && compChoice == "rock") {
-    return "It's a Draw";
-  }
-  //paper Choice
-  else if (userChoice == "paper" && compChoice == "rock") {
-    return "You Win! paper beats rock";
-  } else if (userChoice == "paper" && compChoice == "scissors") {
-    return "You Lose! scissors beats paper";
-  } else if (userChoice == "paper" && compChoice == "paper") {
-    return "It's a Draw";
-  }
-  //scissors Choice
-  else if (userChoice == "scissors" && compChoice == "paper") {
-    return "You win! scissors beat paper";
-  } else if (userChoice == "scissors" && compChoice == "rock") {
-    return "You Lose! rock beats scissors";
-  } else if (userChoice == "scissors" && compChoice == "scissors") {
-    return "It's a Draw";
-  } else {
-    return "Something Went Wrong";
-  }
-}
+    if (userChoice === compChoice) {
+        return "Tie game!"
+    } else if (userChoice === "Rock" && compChoice === "Scissors") {
+        playerScore += 1;
+        return `You win! ${userChoice} beats ${compChoice}!`
+        
+    } else if (userChoice === "Paper" && compChoice === "Rock") {
+        playerScore += 1;
+        return `You win! ${userChoice} beats ${compChoice}!`
+        
+    } else if (userChoice === "Scissors" && compChoice === "Paper") {
+        playerScore += 1;
+        return `You win! ${userChoice} beats ${compChoice}!`
 
-console.log(playRound(userChoice(), compChoice()))
-
-function game();
-for (let i = 0; i < 5; i++) {
-    if (playRound === 5) {
-        console.log("End of the game")
+    } else {
+        computerScore += 1;
+        return `You lose! ${compChoice} beats ${userChoice}`
     }
-
 }
+
+function winGame() {
+    if (playerScore === 5) {
+        return "You win!"
+    } else if (computerScore === 5) {
+        return "You lose!"
+    }
+}
+
+game();
+
+console.log(playerScore);
+console.log(computerScore);
